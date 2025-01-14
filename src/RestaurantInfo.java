5d98bb779ec62bdab15fa43c3b149b63112bbb82
craftyarm <justin.calmus@csuglobal.edu>
2025-01-14 12:38:14 -0800

    v1

---
+++ src/RestaurantInfo.java
@@ -0,0 +1,8 @@
+public class RestaurantInfo {
+    public static void main(String[] args) {
+        System.out.println("----- Restaurant Information -----");
+        System.out.println("Restaurant Name: Muracci's Japanese Curry & Grill");
+        System.out.println("Business Address: 307 Kearny Street");
+        System.out.println("City, State, Zip Code: San Francisco, CA 94108");
+    }
+}
